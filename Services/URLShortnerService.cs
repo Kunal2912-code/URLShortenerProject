a6f86d133a83b449e6fa1f50b9e4373bbd17726e
Kunal2912-code <kunal2912prasad@gmail.com>
2025-02-15 13:05:48 +0530

    Changes done and new folder added as well

---
+++ Services/URLShortnerService.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using URLShortenerProject.Model;
+
+namespace URLShortenerProject.Services
+{
+    public class URLShortnerService : IUrlShortnerService
+    {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private readonly Random _random = new();
+
+        public Task<string?> GetOriginalUrlAsync(string shortCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ShortenUrlAsync(string originalUrl)
+        {
+            // validate 
+            // generate shprt URL  / code
+            var shortcode = GenerateShortCode();
+            // add prefix if needed
+            var shortUrl = "newgen.ly" + shortcode;
+            var mapping = new UrlMapping();
+            mapping.ShortUrl = shortUrl;
+            mapping.LongUrl = originalUrl ;
+            // add into database 
+
+            throw new NotImplementedException();
+        }
+
+        private string GenerateShortCode(int length = 6)
+        {
+            return new string(Enumerable.Repeat(Alphabet, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
